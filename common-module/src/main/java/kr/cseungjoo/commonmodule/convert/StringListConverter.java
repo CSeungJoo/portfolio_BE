2462ff79808f6ee5f96bb71bf9f2fd99cd64ca98
CSeungJoo <seungjoo13579@gmail.com>
2024-08-05 20:38:29 +0900

    profile 조회 서비스 메서드 생성및 예외처리 추가

---
+++ common-module/src/main/java/kr/cseungjoo/commonmodule/convert/StringListConverter.java
@@ -0,0 +1,42 @@
+package kr.cseungjoo.commonmodule.convert;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+public class StringListConverter implements AttributeConverter<List<String>, String> {
+
+    private static final ObjectMapper mapper = new ObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false)
+            .configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
+    // DB에 저장 될 때 사용
+    @Override
+    public String convertToDatabaseColumn(List<String> attribute) {
+        try {
+            return mapper.writeValueAsString(attribute);
+        } catch (JsonProcessingException e) {
+            log.debug("StringListConverter.convertToDatabaseColumn exception occur attribute: {}", attribute.toString());
+//            throw new BusinessLogicException(ExceptionCode.UNABLE_TO_CONVERT_LIST_TO_STRING);
+            throw new IllegalStateException();
+        }
+    }
+
+    // DB의 데이터를 Object로 매핑할 때 사용
+    @Override
+    public List<String> convertToEntityAttribute(String dbData) {
+        try {
+            return mapper.readValue(dbData, List.class);
+        } catch (IOException e) {
+            log.debug("StringListConverter.convertToEntityAttribute exception occur dbData: {}", dbData);
+//            throw new BusinessLogicException(ExceptionCode.UNABLE_TO_CONVERT_STRING_TO_LIST);
+            throw new IllegalStateException();
+
+        }
+    }
+}
--- common-module/src/main/java/kr/cseungjoo/commonmodule/exception/ErrorCode.java
+++ common-module/src/main/java/kr/cseungjoo/commonmodule/exception/ErrorCode.java
@@ -6,7 +6,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    JOB_CATEGORY_NOT_FOUND("카테고리를 찾을수 없습니다.", HttpStatus.NOT_FOUND, "7404");
+    //job_category
+    JOB_CATEGORY_NOT_FOUND("카테고리를 찾을 수 없습니다.", HttpStatus.NOT_FOUND, "7404"),
+
+    //profile
+    PROFILE_NOT_FOUND("프로필을 찾을 수 없습니다.", HttpStatus.NOT_FOUND, "1404");
 
     private final String msg;
     private final HttpStatus status;
